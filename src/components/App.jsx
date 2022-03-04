30f76e9c29eaf2918ebddc9a28853286822dc038
Piron-Johny <piron.johny@gmail.comm>
2022-03-04 16:12:26 +0200

    Переписал App на хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,133 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import ImageGallery from './ImageGallery';
 import { fetchMoviesWithQuery } from 'services/api';
 
-class App extends Component {
-  state = {
-    searchValue: '',
-    isPending: false,
-    images: [],
-    page: 1,
-    loader: false,
-  };
+const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(false);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { isPending, searchValue, page } = this.state;
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isPending) {
+        try {
+          const fetchImages = await fetchMoviesWithQuery(searchValue, page);
+          setImages(page > 1 ? [...images, ...fetchImages] : fetchImages);
+          setLoader(false);
+          setIsPending(false);
 
-    if (isPending) {
-      try {
-        const fetchImages = await fetchMoviesWithQuery(searchValue, page);
-        this.setState(({ images }) => ({
-          images: page > 1 ? [...images, ...fetchImages] : fetchImages,
-          loader: false,
-          isPending: false,
-        }));
-
-        if (page > 1) {
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
-          });
+          if (page > 1) {
+            window.scrollTo({
+              top: document.documentElement.scrollHeight,
+              behavior: 'smooth',
+            });
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        this.setState({ error: error.response.error });
       }
-    }
-  }
+    };
+    fetchData();
+  }, [images, isPending, page, searchValue]);
 
-  handleFormSubmit = searchValue => {
-    this.setState({ searchValue, isPending: true, page: 1, loader: true });
+  const handleFormSubmit = searchValue => {
+    setSearchValue(searchValue);
+    setIsPending(true);
+    setPage(1);
+    setLoader(true);
+
     window.scrollTo({
       top: '',
       behavior: 'smooth',
     });
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-      isPending: true,
-      loader: true,
-    }));
+  const onLoadMore = () => {
+    setPage();
+    setIsPending(true);
+    setLoader(true);
   };
 
   // ======================== RENDER ==================================
 
-  render() {
-    const { loader, images } = this.state;
-    return (
-      <>
-        <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          images={images}
-          onLoadMore={this.onLoadMore}
-          loader={loader}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <SearchBar onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} onLoadMore={onLoadMore} loader={loader} />
+    </>
+  );
+};
 
 export default App;
+
+// ===============================================
+// class App extends Component {
+//   state = {
+//     searchValue: '',
+//     isPending: false,
+//     images: [],
+//     page: 1,
+//     loader: false,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { isPending, searchValue, page } = this.state;
+
+//     if (isPending) {
+//       try {
+//         const fetchImages = await fetchMoviesWithQuery(searchValue, page);
+//         this.setState(({ images }) => ({
+//           images: page > 1 ? [...images, ...fetchImages] : fetchImages,
+//           loader: false,
+//           isPending: false,
+//         }));
+
+//         if (page > 1) {
+//           window.scrollTo({
+//             top: document.documentElement.scrollHeight,
+//             behavior: 'smooth',
+//           });
+//         }
+//       } catch (error) {
+//         this.setState({ error: error.response.error });
+//       }
+//     }
+//   }
+
+//   handleFormSubmit = searchValue => {
+//     this.setState({ searchValue, isPending: true, page: 1, loader: true });
+//     window.scrollTo({
+//       top: '',
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   onLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//       isPending: true,
+//       loader: true,
+//     }));
+//   };
+
+//   // ======================== RENDER ==================================
+
+//   render() {
+//     const { loader, images } = this.state;
+//     return (
+//       <>
+//         <SearchBar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery
+//           images={images}
+//           onLoadMore={this.onLoadMore}
+//           loader={loader}
+//         />
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
